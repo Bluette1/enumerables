5b299773c1d04a7c1930adec419116c7b58216f4
Bluette1 <marylene.sawyer@gmail.com>
2020-05-22 17:11:03 +0200

    fixed a few issues
    - fixed a few issues
      - added missing functionality

--- enumerable.rb
+++ enumerable.rb
@@ -1,231 +1,198 @@
 # This implements some enumerable methods similar to the ones that exist in the `Enumerable` module.
-module Enumerable # rubocop:todo Metrics/ModuleLength
+module Enumerable
   def my_each
-    each do |item|
+    return to_enum(:my_each) unless block_given?
+    for item in self
       yield item
     end
   end
 
   def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
     idx = 0
-    each do |item|
+    my_each do |item|
       yield item, idx
       idx += 1
     end
   end
 
   def my_select
+    return to_enum(:my_select) unless block_given?
     selected = []
-    each do |item|
+    my_each do |item|
       selected.push(item) if yield item
     end
     selected
   end
 
-  # rubocop:todo Metrics/PerceivedComplexity
-  # rubocop:todo Metrics/MethodLength
-  def my_all?(*arg) # rubocop:todo Metrics/CyclomaticComplexity
+  def my_all?(arg=nil)
     answer = true
     if block_given?
-      each do |item|
+      my_each do |item|
         unless yield item
-          answer = false
-          break
+            return false
         end
-      end
-    else
-      if arg.empty?
-        each do |item|
-          next unless item.nil? or item === false # rubocop:todo Style/CaseEquality
-
-          answer = false
-          return answer
-        end
-        return answer
-      end
-      if arg[0].is_a?(Class)
-        each do |item|
-          next if item.is_a?(arg[0])
-
-          answer = false
-          return answer
-        end
-        return answer
-      end
-
-      begin
-        each do |item|
-          Regexp.compile(arg[0])
-          next unless (arg[0] =~ item).nil?
-
-          answer = false
-          break
-        end
-      rescue StandardError => e
-        puts 'Invalid regex. Please use a valid regex string', e
-        return
-      end
-
-    end
-
-    answer
-  end
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/PerceivedComplexity
-
-  # rubocop:todo Metrics/PerceivedComplexity
-  # rubocop:todo Metrics/MethodLength
-  def my_any?(*arg) # rubocop:todo Metrics/CyclomaticComplexity
-    answer = false
-    if block_given?
-      each do |item|
-        next unless yield item
-
-        answer = true
-        break
-      end
-    else
-      if arg.empty?
-        each do |item|
-          # rubocop:todo Style/CaseEquality
-          next unless !item.nil? or item === true
-
-          # rubocop:enable Style/CaseEquality
-
-          answer = true
-          return answer
-        end
-        return answer
-      end
-      if arg[0].is_a?(Class)
-        each do |item|
-          next unless item.is_a?(arg[0])
-
-          answer = true
-          return answer
-        end
-        return answer
-      end
-
-      begin
-        each do |item|
-          Regexp.compile(arg[0])
-          next unless arg[0] =~ item
-
-          answer = true
-          break
-        end
-      rescue StandardError => e
-        puts 'Invalid regex. Please use a valid regex string', e
-        return
-      end
-
-    end
-
-    answer
-  end
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/PerceivedComplexity
-
-  # rubocop:todo Metrics/PerceivedComplexity
-  # rubocop:todo Metrics/MethodLength
-  def my_none?(*arg) # rubocop:todo Metrics/CyclomaticComplexity
-    answer = true
-    if block_given?
-      each do |item|
-        next unless yield item
-
-        answer = false
-        break
       end
       return answer
     else
-      if arg.empty?
-        each do |item|
-          # rubocop:todo Style/CaseEquality
-          next if item.nil? or item === false
-
-          # rubocop:enable Style/CaseEquality
-
-          answer = false
-          return answer
+      if arg.nil?
+        my_each do |item|
+          next unless item.nil? or item == false
+          return false
         end
         return answer
       end
-      if arg[0].is_a?(Class)
-        each do |item|
-          next unless item.is_a?(arg[0])
-
-          answer = false
-          return answer
+      if arg.is_a?(Class)
+        my_each do |item|
+          next if item.is_a?(arg)
+          return false
         end
         return answer
       end
-
-      begin
-        each do |item|
-          Regexp.compile(arg[0])
-          next unless arg[0] =~ item
-
-          answer = false
-          break
+      if arg.is_a?(Regexp)
+        my_each do |item|
+            next unless (arg =~ item).nil?
+    
+            return false
         end
-      rescue StandardError => e
-        puts 'Invalid regex. Please use a valid regex string', e
-        return
+        return true
+      end
+       my_each do |item|
+        next if arg.eql? item
+        return false
+       end
+    end
+    answer
+  end
+
+  def my_any?(arg=nil)
+    answer = false
+    if block_given?
+      my_each do |item|
+        next unless yield item
+        return true
+      end
+    else
+      if arg.nil?
+        my_each do |item|
+          next unless !item.nil? or item == true
+          return true
+        end
+        return answer
+      end
+      if arg.is_a?(Class)
+        my_each do |item|
+          next unless item.is_a?(arg)
+          return true
+        end
+        return answer
+      end
+      if arg.is_a?(Regexp)
+        my_each do |item|
+          next unless arg =~ item
+          return true
+        end
+      end
+
+      my_each do |item|
+        next unless arg.eql? item
+        return true
       end
 
     end
-
     answer
   end
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/PerceivedComplexity
 
-  def my_count
+  def my_none?(arg=nil) 
+    answer = true
+    if block_given?
+      my_each do |item|
+        next unless yield item
+        return false
+      end
+      return answer
+    else
+      if arg.nil?
+        my_each do |item|
+          next if item.nil? or item == false
+
+          return false
+        end
+        return answer
+      end
+      if arg.is_a?(Class)
+        my_each do |item|
+          next unless item.is_a?(arg)
+
+          return false
+        end
+        return answer
+      end
+      if arg.is_a?(Regexp)
+        my_each do |item|
+          next unless arg =~ item
+          return false
+        end
+      end
+      my_each do |item|
+        next unless arg.eql? item
+        return false
+      end
+    end
+    answer
+  end
+
+
+  def my_count(arg=nil)
     answer = 0
-    each do |item|
-      next unless yield item
+    if block_given?
+        my_each do |item|
+            next unless yield item
+      
+            answer += 1
+        end
+    elsif not arg.nil?
+        my_each do |item|
+            next unless arg == item
+            answer += 1
+        end
 
-      answer += 1
+    else
+        my_each do |item|
+            answer += 1
+        end
     end
     answer
   end
 
   def my_map
+    return to_enum(:my_map) unless block_given?
     result = []
-    each do |item|
+    my_each do |item|
       result.push yield item
     end
     result
   end
 
   def my_map_accepts_proc(proc)
-    result = []
-    each do |item|
-      result.push proc.call(item)
-    end
-    result
+    return my_map unless proc.is_a?(Proc)
+    my_map &proc
   end
 
-  def my_map_accepts_proc_and_block(proc)
+  def my_map_accepts_proc_and_block(proc, &block)
     return my_map_accepts_proc(proc) if proc.is_a?(Proc)
 
-    result = []
-    each do |item|
-      result.push yield item
-    end
-    result
+    my_map &block
+    
   end
 
-  # rubocop:todo Metrics/PerceivedComplexity
-  # rubocop:todo Metrics/MethodLength
-  def my_inject(*args) # rubocop:todo Metrics/CyclomaticComplexity
+  def my_inject(*args) 
+    return to_enum(:my_inject) unless (block_given? or not args.empty?)
     if args.empty?
       if block_given?
         product = first
         i = 0
-        each do |item|
+        my_each do |item|
           i += 1
           next if i == 1
 
@@ -237,23 +204,23 @@ module Enumerable # rubocop:todo Metrics/ModuleLength
       if block_given?
         product = args[0]
 
-        each do |item|
+        my_each do |item|
           product = yield product, item
         end
         return product
       end
 
       # if symbol is given
-      if args.length === 2 # rubocop:todo Style/CaseEquality
+      if args.length === 2 
         result = args[0]
-        each do |item|
+        my_each do |item|
           result = result.send(args[1], item)
         end
         return result
       end
       result = first
       i = 0
-      each do |item|
+      my_each do |item|
         i += 1
         next if i == 1
 
@@ -263,12 +230,10 @@ module Enumerable # rubocop:todo Metrics/ModuleLength
     end
 
     product = args[0]
-    each do |item|
+    my_each do |item|
       product = yield product, item
     end
 
     product
   end
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/PerceivedComplexity
 end
